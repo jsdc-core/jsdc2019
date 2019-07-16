a703a5f69f0e1d3265306f21ca449f06864f97eb
Ashu Cai <yifangcai@91app.com>
2019-07-16 22:44:23 +0800

    增加議程

--- source/js/agenda.js
+++ source/js/agenda.js
@@ -15,16 +15,16 @@
 
       swalOptions.title = currentSpeakerData.name;
       swalOptions.imageUrl = './images/speakers/' + currentSpeakerData.image;
-      swalOptions.html = '<h4>'+currntAgendaData.title+'</h4>';
+      swalOptions.html = '<h4 class="agenda-title">'+currntAgendaData.title+'</h4>';
       if (currntAgendaData.outline)
         swalOptions.html += '<div class="agenda-outline">' +
           currntAgendaData.outline +
           '</div>';
       else
-        swalOptions.html += '<div class="agenda-outline"><img src="./images/jsdc-mascot-150.jpg"/ style="height: 50px;margin: 10px 10px 10px 0;">噓！講者保密到家。</div>';
+        swalOptions.html += '<div class="agenda-outline">噓！講者保密到家。</div>';
     } else {
-      swalOptions.title = '我是犀牛啦！';
-      swalOptions.imageUrl = './images/jsdc-mascot-150.jpg';
+      // swalOptions.title = '我是犀牛啦！';
+      // swalOptions.imageUrl = './images/jsdc-mascot-150.jpg';
       swalOptions.html = '<h4>晚點告訴大家這位講者是誰</h4>';
     }
 
